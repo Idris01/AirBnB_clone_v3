84dfe5eaffe28d575da247f58c8140807e797cfb
Idris Adebowale <idrys01@gmail.com>
2023-10-02 05:52:38 -0700

    add module level documentation to index.py and refacto code to meet requirements

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""This module define the blueprint of the views"""
 from flask import jsonify
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views import app_views
 
 
 @app_views.route("/status", strict_slashes=False)
